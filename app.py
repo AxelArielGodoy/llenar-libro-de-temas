6e9e0728a4fd5b8d4c866f39f02097a0bd7b348a
Axel Ariel Godoy <axelariel78@gmail.com>
2024-09-07 23:50:30 -0300

    Arreglo el crud y tablas

--- app.py
+++ app.py
@@ -49,21 +49,42 @@ def crear():
     return redirect(url_for('index'))
 
 # Ruta para actualizar un dato existente
+# @app.route('/actualizar/<int:id>', methods=['POST'])
+# def actualizar(id):
+#     for dato in datos:
+#         if dato['id'] == id:
+
+#             dato.update({
+#                 "dia": request.form['dia'],
+#                 "fecha": request.form['fecha'],
+#                 "mes": request.form['mes'],
+#                 "unidad_numero": request.form['unidad_numero'],
+#                 "clase_numero": request.form['clase_numero'],
+#                 "curso": request.form['curso'],
+#                 "caracter_clase": request.form['caracter_clase'],
+#                 "contenidos_tematicos": request.form['contenidos_tematicos'],
+#                 "actividades": request.form['actividades'],
+#                 "observaciones": request.form['observaciones']
+#             })
+#             break
+#     guardar_datos()  # Guarda en el archivo Excel después de actualizar
+#     return redirect(url_for('index'))
+
 @app.route('/actualizar/<int:id>', methods=['POST'])
 def actualizar(id):
     for dato in datos:
         if dato['id'] == id:
             dato.update({
-                "dia": request.form['dia'],
-                "fecha": request.form['fecha'],
-                "mes": request.form['mes'],
-                "unidad_numero": request.form['unidad_numero'],
-                "clase_numero": request.form['clase_numero'],
-                "curso": request.form['curso'],
-                "caracter_clase": request.form['caracter_clase'],
-                "contenidos_tematicos": request.form['contenidos_tematicos'],
-                "actividades": request.form['actividades'],
-                "observaciones": request.form['observaciones']
+                "dia": request.form.get('dia', dato.get('dia', '')),
+                "fecha": request.form.get('fecha', dato.get('fecha', '')),
+                "mes": request.form.get('mes', dato.get('mes', '')),
+                "unidad_numero": request.form.get('unidad_numero', dato.get('unidad_numero', '')),
+                "clase_numero": request.form.get('clase_numero', dato.get('clase_numero', '')),
+                "curso": request.form.get('curso', dato.get('curso', '')),
+                "caracter_clase": request.form.get('caracter_clase', dato.get('caracter_clase', '')),
+                "contenidos_tematicos": request.form.get('contenidos_tematicos', dato.get('contenidos_tematicos', '')),
+                "actividades": request.form.get('actividades', dato.get('actividades', '')),
+                "observaciones": request.form.get('observaciones', dato.get('observaciones', ''))
             })
             break
     guardar_datos()  # Guarda en el archivo Excel después de actualizar
