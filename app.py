d225dc3069c41471d9c0e06b2e5d995e5ebd7c87
Axel Ariel Godoy <axelariel78@gmail.com>
2024-09-12 23:05:04 -0300

    buscando el responsive

--- app.py
+++ app.py
@@ -78,4 +78,4 @@ def descargar_excel():
     return send_file(EXCEL_FILE, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(app.run(host='0.0.0.0'), debug=True)
